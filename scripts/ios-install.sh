455fb5187c796963fe11eb26ec53cba82df41923
sowmya-kiot <sowmya@kiot.io>
2023-11-29 15:04:15 +0530

    [new]-ios ble fix release

--- scripts/ios-install.sh
+++ scripts/ios-install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cp -a "$1"/plugins/cordova-plugin-ble-central/src/ios/TelinkSigMeshLib "$1"/platforms/ios/
+cp -a "$1"/plugins/kiot-cordova-plugin-ble-central/src/ios/TelinkSigMeshLib "$1"/platforms/ios/
