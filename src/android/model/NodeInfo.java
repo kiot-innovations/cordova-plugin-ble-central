a02acd799d092562ab8a5f7bc6ceba54451281ca
monty python <riteeshreddy333@gmail.com>
2024-11-11 15:42:38 +0530

    [update] v4.1.0.1 changes

--- src/android/model/NodeInfo.java
+++ src/android/model/NodeInfo.java
@@ -20,7 +20,7 @@
  *          See the License for the specific language governing permissions and
  *          limitations under the License.
  *******************************************************************************************************/
-package com.telink.ble.mesh.model;
+package com.megster.cordova.ble.central.model;
 
 import android.os.Handler;
 import android.util.SparseArray;
